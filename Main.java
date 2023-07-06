e28aa2a9e512cee00aa16b06a343e651e06fa40e
mariiaz2306 <135419670+mariiaz2306@users.noreply.github.com>
2023-07-06 22:18:08 +0200

    Add files via upload
    hometask java 04.07.

---
+++ Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char mychar = 'G';
+        int myint = 89;
+        byte mybyte = 4;
+        short myshort = 56;
+        float myfloat = 4.7333436f;
+        double mydouble = 4.355453532d;
+        long mylong = 12121;
+
+        Character myCharacter = 'G';
+        Integer myInteger = 89;
+        Byte myByteObject = 4;
+        Short myShort = 56;
+        Float myFloatObject = 4.7333436f;
+        Double myDoubleObject = 4.355453532d;
+        Long myLongObject = 12121L;
+
+        System.out.println("char:" + mychar);
+        System.out.println("int:" + myint);
+        System.out.println("byte:" + mybyte);
+        System.out.println("short:" + myshort);
+        System.out.println("float:" + myfloat);
+        System.out.println("double:" + myfloat);
+        System.out.println("long:" + mylong);
+
+        System.out.println("Character:" + myCharacter);
+        System.out.println("Integer:" + myInteger);
+        System.out.println("Byte:" + myByteObject);
+        System.out.println("Short:" + myShort);
+        System.out.println("Float:" + myFloatObject);
+        System.out.println("Double:" + myDoubleObject);
+        System.out.println("Long:" +myLongObject);
+    }
+}
